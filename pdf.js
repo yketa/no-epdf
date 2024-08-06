6c88210b24d6150ae754adbed3a95cdb396d233b
Yann-Edwin Keta <yketa@posteo.net>
2024-08-06 11:16:16 +0200

    epub viewer

--- pdf.js
+++ pdf.js
@@ -1,5 +1,7 @@
 if ( document.querySelector('html').id == "simpleViewer" ) { // id of <html> should be "simpleViewer"
-	var newurl = window.location.href.replace("/epdf/", "/pdf/");
+	var newurl = window.location.href
+        .replace("/epdf/", "/pdf/")
+        .replace("/epub/", "/pdf/");
 	if ( confirm("Open " + newurl + "?") ) {
 		window.location.href = newurl;
 	}
